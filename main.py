f18cb91d355ca5e68305ddddb3cffb34db7b78b3
Avi Aryan <avi.aryan123@gmail.com>
2017-07-01 18:17:06 +0530

    solve q3

--- main.py
+++ main.py
@@ -16,6 +16,20 @@ on concat('/article/', articles.slug) = log.path where
 log.status like '%200%' group by authors.name order by views desc
 """
 
+ques_3 = 'On which days did more than 1% of requests lead to errors?'
+query_3 = """
+select * from (
+    select a.day,
+    round(cast((100*b.hits) as numeric) / cast(a.hits as numeric), 2)
+    as errp from
+        (select date(time) as day, count(*) as hits from log group by day) as a
+        inner join
+        (select date(time) as day, count(*) as hits from log where status
+        like '%404%' group by day) as b
+    on a.day = b.day)
+as t where errp > 1.0;
+"""
+
 
 class Problem:
     def __init__(self):
@@ -29,12 +43,12 @@ class Problem:
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
-    def solve(self, ques, query):
+    def solve(self, ques, query, suffix='views'):
         query = query.replace('\n', ' ')
         result = self.execute_query(query)
         print ques
         for i in range(len(result)):
-            print '\t', i + 1, '.', result[i][0], '--', result[i][1]
+            print '\t', i + 1, '.', result[i][0], '--', result[i][1], suffix
         # blank line
         print ''
 
@@ -46,4 +60,5 @@ if __name__ == '__main__':
     problem = Problem()
     problem.solve(ques_1, query_1)
     problem.solve(ques_2, query_2)
+    problem.solve(ques_3, query_3, '% error')
     problem.exit()
